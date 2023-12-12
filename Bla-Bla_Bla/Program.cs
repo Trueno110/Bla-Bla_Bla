c44bbf10ff5d57e0f94f31765b660e3e98c557d6
Trueno110 <Trueno110@yandex.ru>
2023-12-12 11:40:03 +0300

    Добавьте файлы проекта.

---
+++ Bla-Bla_Bla/Program.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bla_Bla_Bla
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Zadanie_5();
+        }
+
+        private static void Zadanie_1()
+        {
+            int[] array = { 6, 23, 3, 8, 19, 54, 21, 666, 7, 8734, 5, 2, 111, 78, 96 };
+
+            int Chet = 0;
+            int Nechet = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] % 2 == 0)
+                {
+                    Chet++;
+                }
+                else if (array[i] % 2 != 0)
+                {
+                    Nechet++;
+                }
+            }
+
+
+            Console.WriteLine("Количество четных элементов: " + Chet);
+            Console.WriteLine("Количество нечетных элементов: " + Nechet);
+
+            Console.ReadLine();
+        }
+
+
+        private static void Zadanie_2()
+        {
+            Console.WriteLine("Введите размер массива:");
+            int size = Convert.ToInt32(Console.ReadLine());
+
+            int[] array = new int[size];
+            Console.WriteLine("Введите размер чисел:");
+
+            for (int i = 0; i < size; i++)
+            {
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine("Введите значение для фильтрации:");
+            int filterValue = Convert.ToInt32(Console.ReadLine());
+
+            int count = 0;
+            foreach (int num in array)
+            {
+                if (num < filterValue)
+                {
+                    count++;
+                }
+            }
+
+            Console.WriteLine("Количество значений меньше " + filterValue + " : " + count);
+        }
+        private static void Zadanie_3()
+        {
+            int[] array = { 17, 62, 5, 34, 4, 7, 76, 5, 89, 7, 6, 5, 4, 4 };
+
+            Console.WriteLine("Введите три числа:");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            int num3 = Convert.ToInt32(Console.ReadLine());
+
+            int count = 0;
+            for (int i = 0; i < array.Length - 2; i++)
+            {
+                if (array[i] == num1 && array[i + 1] == num2 && array[i + 2] == num3)
+                {
+                    count++;
+                }
+            }
+
+            Console.WriteLine("Количество повторений последовательности: " + count);
+        }
+        private static void Zadanie_4()
+        {
+            int[] M = { 17, 2, 23, 4, 6, 14, 89, 25, 876 };
+            int[] N = { 36, 4, 53, 6, 17, 236, 78, 25, 14 };
+
+
+            int[] commonElements = M.Intersect(N).Distinct().ToArray();
+
+            Console.WriteLine("Общие элементы без повторений:");
+            foreach (var element in commonElements)
+            {
+                Console.WriteLine(element);
+            }
+
+        }
+        private static void Zadanie_5()
+        {
+            int[,] array = new int[,] { { 1, 7, 3, 5, 5 }, { 4, 5, 8, 4, 0 } };
+
+            int min = array[0, 0];
+            int max = array[0, 0];
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    if (array[i, j] < min)
+                    {
+                        min = array[i, j];
+                    }
+                    if (array[i, j] > max)
+                    {
+                        max = array[i, j];
+                    }
+                }
+            }
+
+            Console.WriteLine("Минимальное значение: " + min);
+            Console.WriteLine("Максимальное значение: " + max);
+        }
+        private static void Zadanie_7()
+        {
+            int[,] array = new int[,] { { 1, 7, 3, 5, 5 }, { 4, 5, 8, 4, 0 } };
+
+            int min = array[0, 0];
+            int max = array[0, 0];
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    if (array[i, j] < min)
+                    {
+                        min = array[i, j];
+                    }
+                    if (array[i, j] > max)
+                    {
+                        max = array[i, j];
+                    }
+                }
+            }
+
+            Console.WriteLine("Минимальное значение: " + min);
+            Console.WriteLine("Максимальное значение: " + max);
+        }
+    }
+}
---
+++ Bla-Bla_Bla/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Bla-Bla_Bla")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Bla-Bla_Bla")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("e50920fb-184a-42a8-8cc4-34bc72394108")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
